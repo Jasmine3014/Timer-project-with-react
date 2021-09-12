9c1be1f90b0f84fd78cfa688846c0982f824c296
jasmine <jasminetripti5@gmail.com>
2021-09-12 21:08:13 +0600

    start,reset,stop btn add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import './index.css'
 class App extends Component {
-
+    // intervalId=null
      state = {
          count:0
      }
@@ -13,9 +13,39 @@ class App extends Component {
          this.setState({count: this.state.count - 1})
          }
       }
+      startTimer = () => {
+        if (this.state.count > 0 && !this.intervalId) {
+        this.intervalId = setInterval(() => {
+          this.setState({ count: this.state.count -1}, () => {
+            if (this.state.count===0){
+              alert('Timer Finished');
+              clearInterval(this.intervalId);
+              this.intervalId = null;
+        
+        }
+        
+        });
+        
+        }, 1000);
+    }  
+}  
+
+        stopTimer = () => {
+
+            if (this.intervalid) {
+            
+            clearInterval(this.intervalId); this.intervalId = null;
+            
+            }
+        }
+        resetTimer = () => {
+
+            this.setState({ count: 0 });
+             clearInterval(this.intervalid);
+            this.intervalid = null;
+        };
+            
   render(){
-
-
   return (
     <div className="home">
         <div className="container">
@@ -24,6 +54,13 @@ class App extends Component {
                 <p className="Text"onClick={this.decrementCount}>-</p>
                 <input type="submit" value={this.state.count}></input>
                 <p className="Text"onClick={this.incrementCount}>+</p>
+           </div>
+           <div className="hero-ctn">
+               <button className="ctn" onClick={this.startTimer}>Start</button>
+               <button className="ctn" onClick={this.resetTimer}>Restart</button>
+               <button className="ctn" onClick={this.stopTimer}>Stop</button>
+
+
            </div>
         </div>
     </div>
