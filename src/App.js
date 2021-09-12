b4208fd04d9b28ffdac663ecc58fa9fcb7aef372
jasmine <jasminetripti5@gmail.com>
2021-09-12 20:22:04 +0600

    css and condition add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import './index.css'
 class App extends Component {
 
      state = {
@@ -8,19 +9,22 @@ class App extends Component {
        this.setState({count: this.state.count + 1})
      }
      decrementCount=() =>{
-        this.setState({count: this.state.count - 1})
-
+         if(this.state.count >0){
+         this.setState({count: this.state.count - 1})
+         }
       }
   render(){
 
 
   return (
-    <div className="App">
+    <div className="home">
         <div className="container">
-            <h1>Simple Project</h1>
-            <button onClick={this.decrementCount}> - </button>
-            <p className="Text">{this.state.count}</p>
-            <button onClick={this.incrementCount}> + </button>
+            <h1>Timer Project with react</h1>
+           <div className="btn">
+                <p className="Text"onClick={this.decrementCount}>-</p>
+                <input type="submit" value={this.state.count}></input>
+                <p className="Text"onClick={this.incrementCount}>+</p>
+           </div>
         </div>
     </div>
   );
